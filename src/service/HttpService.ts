f51170914f0691a4c4c20314db4002cfb5b07898
an.nguyen <an.nguyen@heatmob.net>
2023-02-02 11:59:56 +0700

    Fix build error

--- src/service/HttpService.ts
+++ src/service/HttpService.ts
@@ -17,55 +17,44 @@ export interface ApiErrorData {
 }
 
 export const httpGet = async (url: string, config?: AxiosRequestConfig) => {
-    const result = await httpApi.get(url, config).catch((e) => {
-        checkHttpStatus(e.response)
-        if (e.response.status === 403) {
-            window.location.href = '../access'
-        }
-        throw e;
-    })
+    const result = await httpApi.get(url, config)
+    checkHttpStatus(result)
     return result
 
 }
 
 export const httpPost = async (url: string, body: any, config?: AxiosRequestConfig) => {
-    const result = await httpApi.post(url, body, config).catch((e) => {
-        checkHttpStatus(e.response)
-        throw e;
-    })
+
+    const result = await httpApi.post(url, body, config)
+    checkHttpStatus(result)
     return result
 
 }
 export const httpPut = async (url: string, body: any, config?: AxiosRequestConfig) => {
 
-    const result = await httpApi.put(url, body, config).catch((e) => {
-        checkHttpStatus(e.response)
-        throw e;
-    })
+    const result = await httpApi.put(url, body, config)
+    checkHttpStatus(result)
     return result
 
 }
 export const httpDelete = async (url: string, body?: any) => {
-    const result = await httpApi.delete(url, body ?? {}).catch((e) => {
-        checkHttpStatus(e.response)
-        throw e;
-    })
+    const result = await httpApi.delete(url, body ?? {})
+    checkHttpStatus(result)
     return result
 
 }
 
 export const checkHttpStatus = async (result: AxiosResponse<any>) => {
     if (result.status < 200 || result.status >= 400) {
+        NotifyController.warning("Error Network")
         if (result.status === 401) {
             deleteToken()
         }
         if (result.status === 403) {
-            NotifyController.error("No permission")
+            window.location.href = '../access'
         }
         if (result.status >= 500) {
             window.location.href = '../error'
-        } else {
-            NotifyController.error("Error Network")
         }
         return null
     }
